9996d32e7c2d1175be5f486562fb75baa257655c
fromm-finnn <kdc5036@gmail.com>
2022-04-19 16:59:14 +0900

    commit1

---
+++ polls/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HTTPResponse("Hello, Django!")
